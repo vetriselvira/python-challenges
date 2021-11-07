5a677591a042fe5161839084bbe72c4e8e20b5e7
Vetri Rajangam <vetriselvira@gmail.com>
2021-11-07 08:43:42 -0500

    renamed class

--- Edabit/insert_brace_vetri.py
+++ Edabit/insert_brace_vetri.py
@@ -1,7 +1,10 @@
-class menu():
+
+class Menu():
     to_right_count = 0
+
     def __init__(self,list):
         self.list = list
+
     def to_right(self):
         to_right_list = self.list
         if len(to_right_list) - 1 == self.to_right_count:
@@ -11,10 +14,8 @@ class menu():
         self.to_right_count = self.to_right_count + 1
         to_right_list[self.to_right_count] = [to_right_list[self.to_right_count]]
        
-        
-        
 
-p = menu([2,3,4])
+p = Menu([2,3,4])
 p.to_right()
 print(p.list)
 p.to_right()
